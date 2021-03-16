9b29f3129f0fee7cbf12e379ce61a09844514f1d
Mille1992 <michael_schneemann@hotmail.de>
2021-03-16 19:44:38 +0100

    Set up project 5 and layout html

---
+++ project5/yayberhood/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class YayberhoodConfig(AppConfig):
+    name = 'yayberhood'
---
+++ project5/yayberhood/views.py
@@ -0,0 +1,9 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.shortcuts import render
+from django.urls import reverse
+
+# Create your views here.
+def index(request):
+    return render(request, "yayberhood/index.html")
