2b8e43fbab651b6be2a0701f84f5626259ee530b
Mille1992 <michael_schneemann@hotmail.de>
2021-03-20 18:25:58 +0100

    Add pages for sections of webpage. Change scroll bar style

--- project5/yayberhood/urls.py
+++ project5/yayberhood/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("/littleProjects", views.littleProjects, name="littleProjects"),
+    path("/hobbies", views.hobbies, name="hobbies"),
+    path("/borrowIt", views.borrowIt, name="borrowIt"),
+    path("/littleHelpers", views.littleHelpers, name="littleHelpers"),
 ]
--- project5/yayberhood/views.py
+++ project5/yayberhood/views.py
@@ -10,3 +10,12 @@ def index(request):
 
 def littleProjects(request):
     return render(request, "yayberhood/littleProjects.html")
+
+def hobbies(request):
+    return render(request, "yayberhood/hobbies.html")
+
+def borrowIt(request):
+    return render(request, "yayberhood/borrowIt.html")
+    
+def littleHelpers(request):
+    return render(request, "yayberhood/littleHelpers.html")
