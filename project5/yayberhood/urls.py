0a2f1ef99bccdfee0ef5bc74d2fb5c8e5a09265a
Mille1992 <michael_schneemann@hotmail.de>
2021-04-10 11:51:39 +0200

    Add User registration, login and logout

--- project5/yayberhood/urls.py
+++ project5/yayberhood/urls.py
@@ -4,8 +4,11 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("/littleProjects", views.littleProjects, name="littleProjects"),
-    path("/hobbies", views.hobbies, name="hobbies"),
-    path("/borrowIt", views.borrowIt, name="borrowIt"),
-    path("/littleHelpers", views.littleHelpers, name="littleHelpers"),
+    path("littleProjects", views.littleProjects, name="littleProjects"),
+    path("hobbies", views.hobbies, name="hobbies"),
+    path("borrowIt", views.borrowIt, name="borrowIt"),
+    path("littleHelpers", views.littleHelpers, name="littleHelpers"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
 ]
--- project5/yayberhood/views.py
+++ project5/yayberhood/views.py
@@ -3,6 +3,7 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
+from .models import *
 
 # Create your views here.
 def index(request):
@@ -19,3 +20,53 @@ def borrowIt(request):
     
 def littleHelpers(request):
     return render(request, "yayberhood/littleHelpers.html")
+
+def login_view(request):
+    if request.method == "POST":
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "yayberhood/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "yayberhood/login.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        profilePhotoUrl = request.POST["profilePhotoUrl"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "yayberhood/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password, profilePhotoUrl = profilePhotoUrl)
+            user.save()
+        except IntegrityError:
+            return render(request, "yayberhood/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "yayberhood/register.html")
