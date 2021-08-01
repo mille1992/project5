a3aac5bcdb3a56bf95c69c00679f0001bbef3be1
Mille1992 <michael_schneemann@hotmail.de>
2021-08-01 18:24:44 +0200

    add profile page with inbox, outbox, involvements in different parts of webpage

--- project5/yayberhood/urls.py
+++ project5/yayberhood/urls.py
@@ -4,14 +4,21 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+
     path("littleProjects", views.littleProjects, name="littleProjects"),
     path("createLittleProject", views.createLittleProject, name="createLittleProject"),
+
     path("hobbies", views.hobbies, name="hobbies"),
     path("createHobbyGroup", views.createHobbyGroup, name="createHobbyGroup"),
+
     path("borrowIt", views.borrowIt, name="borrowIt"),
     path("createBorrowIt", views.createBorrowIt, name="createBorrowIt"),
+
     path("littleHelpers", views.littleHelpers, name="littleHelpers"),
     path("createLittleHelpers", views.createLittleHelpers, name="createLittleHelpers"),
+
+    path("profilePage", views.profilePage_view, name="profilePage"),
+
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
--- project5/yayberhood/views.py
+++ project5/yayberhood/views.py
@@ -11,6 +11,8 @@ import json
 def index(request):
     return render(request, "yayberhood/landingPage.html")
 
+
+
 def littleProjects(request):
     if request.method == "POST":
         request_data = json.loads(request.body)
@@ -51,6 +53,8 @@ def createLittleProject(request):
         return HttpResponseRedirect(reverse("littleProjects"))
     else:
         return render(request, "yayberhood/createLittleProject.html")
+
+
 
 def hobbies(request):
     if request.method == "POST":
@@ -108,6 +112,8 @@ def createHobbyGroup(request):
         return HttpResponseRedirect(reverse("hobbies"))
     return render(request, "yayberhood/createHobbyGroup.html")
 
+
+
 def borrowIt(request):
     rentals = Rental.objects.all()
     rentals = rentals.order_by("-rentalCreationTimestamp").all()
@@ -135,10 +141,13 @@ def createBorrowIt(request):
         return HttpResponseRedirect(reverse("borrowIt"))
     return render(request, "yayberhood/createBorrowIt.html")
     
+
+
 def littleHelpers(request):
     if request.method == "POST":
         curr_user = request.user
         post_owner = request.POST["littleHelpers-helpPostOwner"]
+        print(post_owner)
         message_content = request.POST["littleHelpers-message"]
         message_type = request.POST["littleHelpers-messageType"]
         
@@ -179,6 +188,28 @@ def createLittleHelpers(request):
         new_help.save()
         return HttpResponseRedirect(reverse("littleHelpers"))
     return render(request, "yayberhood/createLittleHelpers.html")
+
+
+def profilePage_view(request):
+    curr_user = request.user
+    outbox = curr_user.messageOwnerships.all()
+    inbox = curr_user.recipientOfMessages.all()
+
+    little_project_participations = check_user_participations_projects(request)
+    hobby_participations = check_user_participations_hobbies(request)
+    little_helper_participations = check_user_participations_helps(request)
+    borrow_it_participations = check_user_participations_rentals(request)
+
+    return render(request,"yayberhood/profilePage.html", {
+        "curr_user": curr_user,
+        "inbox": inbox,
+        "outbox": outbox,
+        "project_participations": little_project_participations,
+        "hobby_participations": hobby_participations,
+        "little_helper_participations": little_helper_participations,
+        "borrow_it_participations": borrow_it_participations
+        })
+
 
 def login_view(request):
     if request.method == "POST":
@@ -229,3 +260,85 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "yayberhood/register.html")
+
+
+def check_user_participations_projects(request):
+    curr_user = request.user
+
+    project_ownerships = curr_user.projectOwnerships.all()
+    project_memberships = curr_user.projectMemberships.all()
+    project_adminships = curr_user.projectsWithAdminRightsForThisUser.all()
+
+    
+    project_participations = []
+    for project_participation in project_ownerships:
+        project_info = [project_participation.projectName, "Owner"]
+        project_participations.append(project_info)
+
+    for project_participation in project_memberships:
+        project_info = [project_participation.projectName, "Member"]
+        project_participations.append(project_info)
+
+    for project_participation in project_adminships:
+        project_info = [project_participation.projectName, "Admin"]
+        project_participations.append(project_info)
+    project_participations.sort()
+
+    return project_participations
+
+
+def check_user_participations_hobbies(request):
+    curr_user = request.user
+
+    hobbyGroup_ownerships = curr_user.hobbyGroupOwnerships.all()
+    hobbyGroup_memberships = curr_user.hobbyGroupMemberships.all()
+    hobbyGroup_adminships = curr_user.hobbyGroupsWithAdminRightsForThisUser.all()
+
+    
+    hobbyGroup_participations = []
+    for hobbyGroup_participation in hobbyGroup_ownerships:
+        hobbyGroup_info = [hobbyGroup_participation.hobbyGroupName, "Owner"]
+        hobbyGroup_participations.append(hobbyGroup_info)
+
+    for hobbyGroup_participation in hobbyGroup_memberships:
+        hobbyGroup_info = [hobbyGroup_participation.hobbyGroupName, "Member"]
+        hobbyGroup_participations.append(hobbyGroup_info)
+
+    for hobbyGroup_participation in hobbyGroup_adminships:
+        hobbyGroup_info = [hobbyGroup_participation.hobbyGroupName, "Admin"]
+        hobbyGroup_participations.append(hobbyGroup_info)
+    hobbyGroup_participations.sort()
+
+    return hobbyGroup_participations
+
+def check_user_participations_helps(request):
+    curr_user = request.user
+
+    help_ownerships = curr_user.helpOwnerships.all()
+    
+    help_participations = []
+    for help_participation in help_ownerships:
+        help_info = [help_participation.helpTitle, "Owner", help_participation.helpType]
+        help_participations.append(help_info)
+    help_participations.sort()
+
+    return help_participations
+
+def check_user_participations_rentals(request):
+    curr_user = request.user
+
+    rental_ownerships = curr_user.rentalsOwnerships.all()
+    rental_borrowed = curr_user.rentalsBorrowedByUser.all()
+    
+    rental_participations = []
+    for rental_participation in rental_ownerships:
+        rental_info = [rental_participation.rentalTitle, "Owner"]
+        rental_participations.append(rental_info)
+
+    for rental_participation in rental_borrowed:
+        rental_info = [rental_participation.rentalTitle, "Borrower"]
+        rental_participations.append(rental_info)
+
+    rental_participations.sort()
+
+    return rental_participations
